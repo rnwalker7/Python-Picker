350cfa249fcac80dda93d9434b682b57d6263ea9
rnwalker7 <rnwalker7@gmail.com>
2021-02-01 10:56:47 -0600

    Updated script to allow for command-line arguments
    CLI right now only allows to choose 3 days; example is python python-picker.py 3 to choose for CWS.  Will add extra CLI options in future releases.

--- pick-names.py
+++ pick-names.py
@@ -1,5 +1,6 @@
 import json
 import random
+import sys
 
 myFile = open('roster.json')
 data = json.load(myFile)
@@ -11,10 +12,15 @@ for names in data:
     if names["available"] == True:
         newData.append(names)
 
-x = random.randint(0, (len(newData) - 1))
+random.seed()
 
+if len(sys.argv) > 1:
+    if sys.argv[1] == "3":
+        x = random.randint(0, (len(newData) - 1))
+        print("Thursday: " + newData[x]["name"])
+
+x = random.randint(0, (len(newData) - 1))
 print("Friday: " + newData[x]["name"])
 
 x = random.randint(0, (len(newData) - 1))
-
 print("Saturday: " + newData[x]["name"])
