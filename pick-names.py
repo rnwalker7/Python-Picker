746c35da966ee4eb9a65988f9aba0e37ea49cd85
rnwalker7 <rnwalker7@gmail.com>
2022-10-11 14:47:11 -0500

    Code cleanup

--- pick-names.py
+++ pick-names.py
@@ -21,36 +21,3 @@ print(len(myNames))
 with open("uub.txt", "w") as f:
     for names in myNames:
         f.write("%s\n" % names)
-
-    # try:
-    #     x = names.text
-    #     print(x)
-    #     break
-    # except Exception as err1:
-    #     pass
-    # myNames.append(x)
-
-# print(myNames)
-
-# myFile = open('roster.json')
-# data = json.load(myFile)
-# myFile.close()
-
-# newData = []
-
-# for names in data:
-#     if names["available"] == True:
-#         newData.append(names)
-
-# random.seed()
-
-# if len(sys.argv) > 1:
-#     if sys.argv[1] == "3":
-#         x = random.randint(0, (len(newData) - 1))
-#         print("Thursday: " + newData[x]["name"])
-
-# x = random.randint(0, (len(newData) - 1))
-# print("Friday: " + newData[x]["name"])
-
-# x = random.randint(0, (len(newData) - 1))
-# print("Saturday: " + newData[x]["name"])
